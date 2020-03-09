98a0b1e610bb1a030c3de847edec04d59633bd24
neikoe <bagusramadhanandhika@gmail.com>
2020-03-09 16:59:52 +0700

    inisialisasi project

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,18 @@
 class Calculator
 {
-	static void Main(string[] args)
-	{
+		static void Main(string[] args)
+		{
 	
+		}
 	}
-}
 
-static int Penambahan(int a, int b)
-{
-	return a + b;
-}
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+	{
